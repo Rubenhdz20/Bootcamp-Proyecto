308c8558ac9dfca8c258ad0b3de7aaea9abad48b
Rubenhdz20 <rubhernandez42@gmail.com>
2024-03-26 23:55:00 -0600

    Add image

---
+++ Bootcamp-Proyecto/src/components/MobileMenu.tsx
@@ -0,0 +1,4 @@
+
+const MobileMenu = () => {}
+
+export default MobileMenu;
---
+++ Bootcamp-Proyecto/src/views/HeroSection.tsx
@@ -0,0 +1,47 @@
+
+
+const HeroSection = () => {
+    return(
+        <>
+            <div className="w-full h-full">
+                <h2>MyFitnessWeb</h2>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Welcome to your free meal tracking and health information tool</h1>
+                    <p>Get started with our free tool for tracking meals, healthy eating information, recipes, meal registration, and nutritional analysis. Improve your health and wellbeing.</p>
+                </div>
+            </div>
+            <div>
+                <h2>Free tools for your health journey</h2>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Track Meals</p>
+                        <p>Record what you eat</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Health Information</p>
+                        <p>Learn about health topics</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Recipes</p>
+                        <p>Find new dishes</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Nutrition Analysis</p>
+                        <p>Understand what you eat</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h2>Change your life now!</h2>
+                <button>Start now</button>
+            </div>
+        </>
+    )
+}
+
+export default HeroSection;
