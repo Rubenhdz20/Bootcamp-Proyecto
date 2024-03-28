f62ece32339ce360fe5a1d44cd73865af3fad4cc
Rubenhdz20 <rubhernandez42@gmail.com>
2024-03-27 23:56:19 -0600

    Styling nav bar

--- Bootcamp-Proyecto/src/views/HeroSection.tsx
+++ Bootcamp-Proyecto/src/views/HeroSection.tsx
@@ -1,44 +1,44 @@
 
-
 const HeroSection = () => {
     return(
         <>
             <div className="w-full h-full">
-                <h2>MyFitnessWeb</h2>
                 <div>
-                    <img src="" alt="" />
-                    <h1>Welcome to your free meal tracking and health information tool</h1>
-                    <p>Get started with our free tool for tracking meals, healthy eating information, recipes, meal registration, and nutritional analysis. Improve your health and wellbeing.</p>
-                </div>
-            </div>
-            <div>
-                <h2>Free tools for your health journey</h2>
-                <div>
-                    <div>
-                        <img src="" alt="" />
-                        <p>Track Meals</p>
-                        <p>Record what you eat</p>
-                    </div>
-                    <div>
-                        <img src="" alt="" />
-                        <p>Health Information</p>
-                        <p>Learn about health topics</p>
-                    </div>
-                    <div>
-                        <img src="" alt="" />
-                        <p>Recipes</p>
-                        <p>Find new dishes</p>
-                    </div>
-                    <div>
-                        <img src="" alt="" />
-                        <p>Nutrition Analysis</p>
-                        <p>Understand what you eat</p>
+                    <p className="text-black text-xl font-bold">MyFitnessWeb</p>
+                    <div className="bg-hero-pattern">
+                        <h1 className="text-white font-bold">Welcome to your free meal tracking and health information tool</h1>
+                        <p className="text-white">Get started with our free tool for tracking meals, healthy eating information, recipes, meal registration, and nutritional analysis. Improve your health and wellbeing.</p>
                     </div>
                 </div>
-            </div>
-            <div>
-                <h2>Change your life now!</h2>
-                <button>Start now</button>
+                <div>
+                    <h2 className="text-black">Free tools for your health journey</h2>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                            <p className="text-black">Track Meals</p>
+                            <p>Record what you eat</p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p className="text-black">Health Information</p>
+                            <p className="text-black">Learn about health topics</p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p className="text-black">Recipes</p>
+                            <p className="text-black">Find new dishes</p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p className="text-black">Nutrition Analysis</p>
+                            <p className="text-black">Understand what you eat</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="text-black">Change your life now!</h2>
+                    <button >Start now</button>
+                </div>
             </div>
         </>
     )
--- Bootcamp-Proyecto/tailwind.config.js
+++ Bootcamp-Proyecto/tailwind.config.js
@@ -1,12 +1,35 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      'main-back': '#F7FCFA',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('src/assets/backgroundhero.png')",
+      },
+      height: {
+        '128': '32rem',
+      }
+    }
   },
-  plugins: [],
 }
 
