7b8f449f1522396f4dd8a16c92584456746a0358
JuanPabdl <a00825316@itesm.mx>
2024-04-04 19:43:40 -0600

    fix recipe search request load

--- Bootcamp-Proyecto/src/pages/SearchRecipee.tsx
+++ Bootcamp-Proyecto/src/pages/SearchRecipee.tsx
@@ -1,6 +1,6 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import RecipeeList from "../components/RecipeeList";
-import SearchBar from "../components/SearchBar";
+//import SearchBar from "../components/SearchBar";
 
 import useRecipeStore from "../state/recipeeResults";
 
@@ -13,17 +13,35 @@ const SearchRecipee = () => {
 
     console.log(searchResults)
 
-    const containerRef = useRef()
+    //const containerRef = useRef()
 
-    const handleSearchBarRequest = async(text: string) => {
-        setSearchText(text)
-        console.log(searchText)
+    const handleSearchBarRequest = async() => {
         onSearchResults(searchText)
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        //console.log(e.target.value)
+        setSearchText(e.target.value)
     }
 
  return (
     <div className="flex flex-col flex-nowrap place-content-center mx-auto">
-        <SearchBar onSearch={handleSearchBarRequest} ref={containerRef}/>
+        {/*<SearchBar onSearch={handleSearchBarRequest} ref={containerRef}/>*/}
+        <div className="flex flex-row flex-nowrap place-content-around mb-6 p-4 gap-4">
+            <h2 className="text-3xl font-bold">Recipee Search</h2>
+            <div>
+                <input type="text"
+                placeholder="Find a recipee"
+                onChange={handleInputChange}
+                value={searchText}
+                className="bg-gray-200 
+                text-gray-600 
+                placeholder:text-gray-400 
+                p-2 rounded-lg mb-4"/>
+                <button onClick={handleSearchBarRequest}
+                className='bg-green-500 text-white m-1'>Search</button>
+            </div>
+        </div>
         {isLoading && <div>Cargando recetas...</div>}
         {error && <div>Disculpa, ha ocurrido un error</div>}
         {!isLoading && <RecipeeList recipes={searchResults}/>}
