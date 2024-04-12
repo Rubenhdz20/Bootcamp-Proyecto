53590d123008ca54e6becae43a2770a64b1032ab
JuanPabdl <a00825316@itesm.mx>
2024-04-12 17:47:54 -0600

    implemented recipe details api call

--- Bootcamp-Proyecto/src/components/RecipeeList.tsx
+++ Bootcamp-Proyecto/src/components/RecipeeList.tsx
@@ -2,13 +2,6 @@ import { memo } from "react";
 import RecipeeItem from "./RecipeeItem"
 import { useNavigate } from "react-router-dom";
 
-/*
-type RecipeeListProps = {
-    searchTerm: string,
-    recipees: [],
-}
-*/
-
 const RecipeeList = (props:any) => {
     const recipes = props.recipes
     const navigate = useNavigate()
@@ -16,15 +9,10 @@ const RecipeeList = (props:any) => {
     const handleRecipeeItemClick = (id:string) => { //Redirige a la página de la receta seleccionada
         navigate(`/detail/${id}`)
     }
-    /*
-    const renderRecipees = () => { //Carga la lista de recetas
-        return
-    }
-    */
    
     return (
         <div className="flex flex-col flex-nowrap">
-            <h2 className="text-2xl font-bold ml-4 place-self-start">Popular Recipees</h2>
+            <h2 className="text-2xl font-bold ml-4 place-self-start">Popular Recipes</h2>
             <div className="flex flex-row flex-wrap place-content-center p-3 lg:grid lg:grid-cols-2">
                 {recipes.map((recipeItem:any,index:any)=>(
                     <RecipeeItem key={`recipe-item-${index}`} name={recipeItem.recipe.label} 
---
+++ Bootcamp-Proyecto/src/utils/constants/index.ts
@@ -0,0 +1,3 @@
+//Constants File 
+//API BASE URL
+export const BASE_URL = 'https://api.edamam.com/api/recipes/v2';
