f0714c2481733efc5db43dd49988cdb7711469f4
JuanPabdl <a00825316@itesm.mx>
2024-04-28 13:59:53 -0600

    update recipe item test & coverage

--- Bootcamp-Proyecto/src/components/RecipeeItem.test.tsx
+++ Bootcamp-Proyecto/src/components/RecipeeItem.test.tsx
@@ -1,38 +1,49 @@
-import { beforeEach, describe, expect, test, vi } from 'vitest'
-import { render, screen} from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event';
 import RecipeeItem from './RecipeeItem'
 
 describe(RecipeeItem, () => {
     const handleClick = vi.fn()
 
-    beforeEach(()=>{
+    test("Render base component", () => {
         render(
             <RecipeeItem 
-                data-testid='recipe-1'
+                data-testid="recipe-1"
                 id='wdfgjgreqr'
                 name='Fried Chicken'
                 imageUrl='realImageUrl.com'
-                prepTime={0}
+                prepTime={10}
                 calories={2500.17}
                 mealType='lunch/dinner'
                 onEventClick={handleClick}
                 />
         )
-    })
-    test("Render base component", () => {
         expect(screen.getByText('Fried Chicken')).toBeDefined()
         expect(screen.getByText('lunch/dinner')).toBeDefined()
         expect(screen.getByText('2500.17 cals')).toBeDefined()
+        expect(screen.getByText('10 mins')).toBeDefined()
     });
 
     test("Render with prepTime as 0", () => {
+        render(
+            <RecipeeItem 
+                data-testid='recipe-2'
+                id='wdfgjgreqr'
+                name='Turkey Sandwich'
+                imageUrl='realImageUrl.com'
+                prepTime={0}
+                calories={2500.17}
+                mealType='lunch/dinner'
+                />
+        )
         expect(screen.queryByText('mins')).toBeNull()
     });
 
     //Resolver como hacer test del click event
     test("Click recipe to navigate details page", () => {
-        userEvent.click(screen.getByTestId('recipe-1'))
-        expect(handleClick).toBeCalledTimes(1)
-    })
+        const item = screen.queryByTestId('recipe-1')
+        userEvent.click(item)
+        expect(handleClick).toHaveBeenCalled()
+    });
 })
