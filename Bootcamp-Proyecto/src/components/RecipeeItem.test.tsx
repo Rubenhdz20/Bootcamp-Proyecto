b43dbf7efcda1dc4353b8924930d429951d6f289
JuanPabdl <a00825316@itesm.mx>
2024-04-28 16:52:16 -0600

    fix recipe item test

--- Bootcamp-Proyecto/src/components/RecipeeItem.test.tsx
+++ Bootcamp-Proyecto/src/components/RecipeeItem.test.tsx
@@ -13,7 +13,7 @@ describe(RecipeeItem, () => {
                 id='wdfgjgreqr'
                 name='Fried Chicken'
                 imageUrl='realImageUrl.com'
-                prepTime={10}
+                prepTime={0}
                 calories={2500.17}
                 mealType='lunch/dinner'
                 onEventClick={handleClick}
@@ -22,28 +22,18 @@ describe(RecipeeItem, () => {
         expect(screen.getByText('Fried Chicken')).toBeDefined()
         expect(screen.getByText('lunch/dinner')).toBeDefined()
         expect(screen.getByText('2500.17 cals')).toBeDefined()
-        expect(screen.getByText('10 mins')).toBeDefined()
     });
 
     test("Render with prepTime as 0", () => {
-        render(
-            <RecipeeItem 
-                data-testid='recipe-2'
-                id='wdfgjgreqr'
-                name='Turkey Sandwich'
-                imageUrl='realImageUrl.com'
-                prepTime={0}
-                calories={2500.17}
-                mealType='lunch/dinner'
-                />
-        )
         expect(screen.queryByText('mins')).toBeNull()
     });
 
     //Resolver como hacer test del click event
-    test("Click recipe to navigate details page", () => {
-        const item = screen.queryByTestId('recipe-1')
-        userEvent.click(item)
-        expect(handleClick).toHaveBeenCalled()
+    test("Click recipe to navigate details page", async () => {
+        const user = userEvent.setup()
+        const item = screen.getByText('See more')
+
+        await user.click(item)
+        expect(handleClick).toBeCalledTimes(1)
     });
 })
