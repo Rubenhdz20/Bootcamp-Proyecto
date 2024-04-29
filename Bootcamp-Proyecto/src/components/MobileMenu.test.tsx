07bd7c4d55770ceb0d5453f45392e603f1749faf
JuanPabdl <a00825316@itesm.mx>
2024-04-29 13:04:04 -0600

    test mobile menu links

---
+++ Bootcamp-Proyecto/src/components/MobileMenu.test.tsx
@@ -0,0 +1,48 @@
+import { test, expect, describe } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+
+import MobileMenu from "./MobileMenu";
+
+describe(MobileMenu,() => {
+    const router = createMemoryRouter([
+        {
+            path:'/', 
+            element:<MobileMenu/>
+        }, {
+            path:'/meal-loggin',
+            element: <div>New Meal</div>
+        }, {
+            path:'/search-recipe',
+            element: <div>Search Recipes</div>
+        }
+    ])
+
+    test('Render mobile menu', () => {
+        render(
+            <RouterProvider router={router}/>
+        )
+        const menu = screen.getByRole('navigation')
+        expect(menu).toBeDefined()
+        expect(screen.getByText('Home')).toBeDefined()
+        expect(screen.getByText('Recipes')).toBeDefined()
+        expect(screen.getByText('Meal Loggin')).toBeDefined()
+    });
+
+    test('Menu has right Recipe path', () => {
+        const link = screen.getByRole('link',{name:'Recipes'})
+        expect(link.getAttribute('href')).toBe('/search-recipe')
+    });
+
+    test('Menu has right Meal loggin path', () => {
+        const link = screen.getByRole('link',{name:'Meal Loggin'})
+        expect(link.getAttribute('href')).toBe('/meal-loggin')
+    });
+
+    test('Menu sends to Meal Loggin page on click', ()=>{
+        const link = screen.getByRole('link',{name:'Meal Loggin'})
+        fireEvent.click(link)
+        expect(screen.getByText('New Meal')).toBeDefined()
+    });
+});
+
